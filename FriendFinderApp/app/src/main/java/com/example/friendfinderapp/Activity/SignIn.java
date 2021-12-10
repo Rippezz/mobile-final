0d5a67653ba1f471f8bcccf4592420e58cf27081
Tharixs Akbar <tharixsakbar@gmail.com>
2021-12-10 16:14:10 +0700

    Add Session Login @E41200036

--- FriendFinderApp/app/src/main/java/com/example/friendfinderapp/Activity/SignIn.java
+++ FriendFinderApp/app/src/main/java/com/example/friendfinderapp/Activity/SignIn.java
@@ -1,6 +1,8 @@
 package com.example.friendfinderapp.Activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,7 +27,8 @@ import retrofit2.Response;
 public class SignIn extends AppCompatActivity {
     private EditText etEmail, etPassword;
     String email, password;
-
+    private SharedPreferences sharedPreferences;
+    public static final String MyPref = "login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +37,9 @@ public class SignIn extends AppCompatActivity {
 
         etEmail = findViewById(R.id.signinEmail);
         etPassword = findViewById(R.id.signinPassword);
+
         Button btnLogin = findViewById(R.id.btn_sign_in);
+        sharedPreferences = getSharedPreferences(MyPref, Context.MODE_PRIVATE);
 
         btnLogin.setOnClickListener(v -> {
             email = etEmail.getText().toString();
@@ -44,16 +49,31 @@ public class SignIn extends AppCompatActivity {
             } else if (password.trim().length() == 0) {
                 etPassword.setError("field tidak boleh kosong");
             } else {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("email", email);
+                editor.putString("password" , password);
+                editor.commit();
                 login(email, password);
             }
         });
+
+        String prefEmail = sharedPreferences.getString("email", "");
+        String prefPassword = sharedPreferences.getString("password", "");
+        System.out.println(prefEmail + " : " + prefPassword);
+        if (prefEmail.length() > 0 && prefPassword.length() > 0) {
+            login(prefEmail, prefPassword);
+        }
+
+//        checkSessionLogin();
 
         TextView link_sign_up = findViewById(R.id.link_sign_up);
         link_sign_up.setOnClickListener(v -> {
             Intent intent = new Intent(getApplicationContext(), SignUp.class);
             startActivity(intent);
         });
+
     }
+
 
     private void login(String email, String password) {
         APIRequestData apiData = RetroServer.konekRetro().create(APIRequestData.class);
@@ -67,7 +87,7 @@ public class SignIn extends AppCompatActivity {
                 if (status.equals("1")) {
                     Toast.makeText(getApplicationContext(), "" + pesan, Toast.LENGTH_SHORT).show();
                     getByEmail();
-                }else {
+                } else {
                     Toast.makeText(getApplicationContext(), "Login Gagal ", Toast.LENGTH_SHORT).show();
                 }
                 finish();
@@ -75,7 +95,7 @@ public class SignIn extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<ResponseModel> call, Throwable t) {
-                Toast.makeText(SignIn.this, "Error : " +t ,Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignIn.this, "Error : " + t, Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
@@ -114,7 +134,7 @@ public class SignIn extends AppCompatActivity {
             @Override
             public void onFailure(Call<ResponseModel> call, Throwable t) {
                 reset();
-                Toast.makeText(SignIn.this, "Eror : "+t,  Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignIn.this, "Eror : " + t, Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
--- FriendFinderApp/app/src/main/java/com/example/friendfinderapp/Constants/ConfigurationAll.java
+++ FriendFinderApp/app/src/main/java/com/example/friendfinderapp/Constants/ConfigurationAll.java
@@ -3,7 +3,7 @@ package com.example.friendfinderapp.Constants;
 public class ConfigurationAll {
 
     public static String user_id = "";
-    static String ip = "10.128.108.209";
+    static String ip = "10.212.151.247";
     public static final String baseUrl = "http://" + ip + "/friend-finder/public/";
 
     public static String ImageURL = "http://" + ip + "/friend-finder/public/img/";
