e5dbaa713f3a834474d39522eef919102c5a23f3
Rifqi Febri Vandhika <gtgrifqi@gmail.com>
2021-12-11 18:45:43 +0700

    Update EventForm.java

--- FriendFinderApp/app/src/main/java/com/example/friendfinderapp/EventForm.java
+++ FriendFinderApp/app/src/main/java/com/example/friendfinderapp/EventForm.java
@@ -36,7 +36,7 @@ public class EventForm extends AppCompatActivity {
                     owner = edeventowner.getText().toString();
                     cp = edeventcp.getText().toString();
                     desc = edeventdesc.getText().toString();
-                    spinner = spinner.get
+                    spinner = spinner.getItemAtPosition();
                 });
 
         ImageView btn_back_to_user_event = findViewById(R.id.btn_back_to_user_event);
