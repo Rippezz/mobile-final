02338c25fe0a4db9f3a38a750d3bd5ceb418966e
Rifqi Febri Vandhika <gtgrifqi@gmail.com>
2021-12-11 13:52:10 +0700

    Update EventForm.java

--- FriendFinderApp/app/src/main/java/com/example/friendfinderapp/EventForm.java
+++ FriendFinderApp/app/src/main/java/com/example/friendfinderapp/EventForm.java
@@ -3,6 +3,8 @@ package com.example.friendfinderapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
@@ -12,6 +14,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class EventForm extends AppCompatActivity {
     private Spinner spinner;
     private String[] list_category = {"Sport", "Education", "Game"};
+    private Button btnTambahData;
+    private String eventname,owner,cp,desc;
+    private EditText edeventname,edeventowner,edeventcp,edeventdesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,19 @@ public class EventForm extends AppCompatActivity {
         spinner = findViewById(R.id.cb_category);
         ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, list_category);
         spinner.setAdapter(arrayAdapter);
+        btnTambahData = findViewById(R.id.tambahdata);
+        edeventname = findViewById(R.id.et_event_name);
+        edeventowner = findViewById(R.id.et_event_owner);
+        edeventcp = findViewById(R.id.et_contact_person);
+        edeventdesc = findViewById(R.id.et_description);
+
+        btnTambahData.setOnClickListener(v -> {
+                    eventname = edeventname.getText().toString();
+                    owner = edeventowner.getText().toString();
+                    cp = edeventcp.getText().toString();
+                    desc = edeventdesc.getText().toString();
+                    spinner = spinner.get
+                });
 
         ImageView btn_back_to_user_event = findViewById(R.id.btn_back_to_user_event);
 
