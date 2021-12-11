8b114fc4844bc5a8e7c133e1a0790812f830f8c9
Rifqi Febri Vandhika <gtgrifqi@gmail.com>
2021-12-11 08:21:01 +0700

    event form

--- FriendFinderApp/app/src/main/java/com/example/friendfinderapp/Constants/ConfigurationAll.java
+++ FriendFinderApp/app/src/main/java/com/example/friendfinderapp/Constants/ConfigurationAll.java
@@ -3,7 +3,7 @@ package com.example.friendfinderapp.Constants;
 public class ConfigurationAll {
 
     public static String user_id = "";
-    static String ip = "10.128.108.32";
+    static String ip = "192.168.1.28";
     public static final String baseUrl = "http://" + ip + "/friend-finder/public/";
 
     public static String ImageURL = "http://" + ip + "/friend-finder/public/img/";
--- FriendFinderApp/app/src/main/java/com/example/friendfinderapp/EventForm.java
+++ FriendFinderApp/app/src/main/java/com/example/friendfinderapp/EventForm.java
@@ -1,9 +1,12 @@
 package com.example.friendfinderapp;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EventForm extends AppCompatActivity {
@@ -17,6 +20,20 @@ public class EventForm extends AppCompatActivity {
         spinner = findViewById(R.id.cb_category);
         ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, list_category);
         spinner.setAdapter(arrayAdapter);
-    }
 
+        ImageView btn_back_to_user_event = findViewById(R.id.btn_back_to_user_event);
+
+        btn_back_to_user_event.setOnClickListener(new View.OnClickListener() {
+            @MainThread
+            public void onBackPressed() {
+                getOnBackPressedDispatcher().onBackPressed();
+            }
+
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
 }
